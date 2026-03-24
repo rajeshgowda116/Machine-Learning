5587c8f29ddc5a954ecd9f53c35a2caa894944ae
Rajesh <rajeshpar2005@gmail.com>
2026-03-24 19:35:58 +0530

    i create current account

--- python/oops/advanceBank.py
+++ python/oops/advanceBank.py
@@ -18,12 +18,23 @@ class Bankaccont:
       print("\n--- Account Details ---")
       print("Name:", self.name)
       print("Account No:", self.acc_no)
-      print("Balance:", self.balance)
+      print("Balance:", self.balence)
   
 class SavingsAccount(Bankaccont):
   def add_interest(self):
     interest=self.balence*0.5
     self.balence+=interest
     print(f"Interest added: ₹{interest}")
+
+class CurrentAccount(Bankaccont):
+  def __init__(self,name, ac_no, balence):
+    super().__init__(self,name,ac_no, balence)
+    self.overdraptlimt=5000
+  def withdraw(self,amount):
+   if amount <= self.balence + self.overdraft_limit:
+      self.balence -= amount
+      print(f"₹{amount} withdrawn (Overdraft allowed).")
+   else:
+      print("Overdraft limit exceeded!")
   
 
