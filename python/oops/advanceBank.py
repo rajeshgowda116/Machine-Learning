99506ca11ad464cbe53816ae644902c9c17279e3
Rajesh <rajeshpar2005@gmail.com>
2026-03-24 19:25:13 +0530

    i create withdraw function

--- python/oops/advanceBank.py
+++ python/oops/advanceBank.py
@@ -3,9 +3,17 @@ class Bankaccont:
     self.name=name
     self.ac_no=ac_no
     self.balence=balence
-    
+
   def deposite(self,amount):
     self.balence+=amount
     print(f"you {amount} is deposite succesfully ")
+  
+  def withdraw(self,amount):
+    if amount<=self.balence:
+      self.belence-=amount
+      print(f"you withdraw {amount} successfully")
+    else:
+      print("insufisient balence")
+
   
 
